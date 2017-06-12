4030f6d60204341323ea49771cfd2614ce0e5488
abxcytf <lawrencelou88@gmail.com>
2017-06-12 14:02:00 -0400

    Create LeetCodeQ208.Implement_Trie (Prefix_Tree).java

--- Week07_Lec/LeetCodeQ208.Implement_Trie (Prefix_Tree).java
+++ Week07_Lec/LeetCodeQ208.Implement_Trie (Prefix_Tree).java
@@ -75,3 +75,77 @@ public class Trie {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
+
+/*************************************************************************************************/
+//HashMap version
+public class Trie {
+    class TrieNode {
+        public char ch;
+        HashMap<Character, TrieNode> children;
+        boolean hasWord;
+        
+        public TrieNode() {
+            this.children = new HashMap<>();
+            this.hasWord = false;
+        }
+        
+        public TrieNode(char ch) {
+            this.ch = ch;
+            this.children = new HashMap<>();
+            this.hasWord = false;
+        }
+        
+    }
+    
+    public TrieNode root;
+    
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        if (word == null || word.length() == 0) {
+            
+        }
+        TrieNode current = root;
+        for (char ch : word.toCharArray()) {
+            if (!current.children.containsKey(ch)) {
+                current.children.put(ch, new TrieNode(ch));
+            }
+            current = current.children.get(ch);
+        }
+        current.hasWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        if (word == null || word.length() == 0) {
+            return false;
+        }
+        TrieNode current = root;
+        for (char ch : word.toCharArray()) {
+            if (!current.children.containsKey(ch)) {
+                return false;
+            }
+            current = current.children.get(ch);
+        }
+        return current.hasWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        if (prefix == null || prefix.length() == 0) {
+            return false;
+        }
+        TrieNode current = root;
+        for (char ch : prefix.toCharArray()) {
+            if (!current.children.containsKey(ch)) {
+                return false;
+            }
+            current = current.children.get(ch);
+        }
+        return true;
+    }
+}
