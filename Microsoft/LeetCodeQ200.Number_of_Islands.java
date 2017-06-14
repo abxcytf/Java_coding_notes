35dc10224de1d210e457e07397e557d9f63616e7
abxcytf <lawrencelou88@gmail.com>
2017-06-14 14:13:46 -0400

    Create LeetCodeQ200.Number_of_Islands.java

---
+++ Microsoft/LeetCodeQ200.Number_of_Islands.java
@@ -0,0 +1,53 @@
+/*
+200. Number of Islands
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+11110
+11010
+11000
+00000
+Answer: 1
+
+Example 2:
+
+11000
+11000
+00100
+00011
+Answer: 3
+*/
+
+
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfsHelper(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    private int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private void dfsHelper(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        for (int[] dir : dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            dfsHelper(grid, x, y);
+        }
+    }
+}
