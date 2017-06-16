5e37c33db8d4c8f7b96e38d29b3c695920f7ef02
abxcytf <lawrencelou88@gmail.com>
2017-06-16 02:13:09 -0400

    Create LeetCodeQ48.Rotate_Image.java

---
+++ Microsoft/LeetCodeQ48.Rotate_Image.java
@@ -0,0 +1,29 @@
+/*
+48. Rotate Image
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Follow up:
+Could you do this in-place?
+*/
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        int len = matrix.length;
+        
+        for (int i = 0; i < len / 2; i++) {
+            for (int j = 0; j < (len + 1) / 2; j++) {
+                int temp = matrix[i][j];
+                
+                matrix[i][j] = matrix[len - j - 1][i];
+                matrix[len - j - 1][i] = matrix[len - i - 1][len - j - 1];
+                matrix[len - i - 1][len - j - 1] = matrix[j][len - i - 1];
+                matrix[j][len - i - 1] = temp;
+            }
+        }
+    }
+}
