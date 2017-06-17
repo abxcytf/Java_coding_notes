9bfab07244bcb2c779d45f3ebb73d27485b24005
abxcytf <lawrencelou88@gmail.com>
2017-06-17 15:19:35 -0400

    Update LeetCodeQ268.Missing_Number.java

--- Microsoft/LeetCodeQ268.Missing_Number.java
+++ Microsoft/LeetCodeQ268.Missing_Number.java
@@ -44,5 +44,31 @@ public class Solution {
     }
     
     /********************************************************************************/
+    //time O(n), space O(1), 数学方法，等差数列求和，首项加末项乘以项数除以2
+    public int missingNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        //等差数列求和
+        int sum = (0 + len) * (len + 1) / 2;
+        for (int i = 0; i < len; i++) {
+            sum -= nums[i];
+        }
+        return sum;
+    }
     
+    /********************************************************************************/
+    //time O(n), space O(1),巧妙运用bit manipulation，相同数字异或为0
+    public int missingNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int result = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= i;
+            result ^= nums[i];
+        }
+        return result;
+    }
 }
