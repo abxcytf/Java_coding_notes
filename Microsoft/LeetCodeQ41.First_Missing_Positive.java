cfb42438c0790f8b008676f491ada1c1619d6ce4
abxcytf <lawrencelou88@gmail.com>
2017-06-17 18:44:42 -0400

    Update LeetCodeQ41.First_Missing_Positive.java

--- Microsoft/LeetCodeQ41.First_Missing_Positive.java
+++ Microsoft/LeetCodeQ41.First_Missing_Positive.java
@@ -44,5 +44,74 @@ public class Solution {
     }
     
     /***********************************************************************************/
+    //time O(n), space O(1)
+    //partition first, then use the scan
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        
+        //partition, 
+        //make nums between [0, nums.length - 1] to the beginning of the input array
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            while (start < nums.length && nums[start] > 0) {
+                start++;
+            }
+            while (end >= 0 && nums[end] <= 0) {
+                end--;
+            }
+            
+            if (start <= end) {
+                swap(nums, start, end);
+            }
+        }
+        
+        for (int i = 0; i < start; i++) {
+            int abs = Math.abs(nums[i]);
+            // if (abs <= nums.length) {
+            //     nums[abs - 1] = - Math.abs(nums[abs - 1]);
+            // }
+            if (abs <= nums.length && nums[abs - 1] > 0) {
+                nums[abs - 1] = -nums[abs - 1];
+            }
+            
+        }
+        
+        for (int i = 0; i < start; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return start + 1;
+    }
     
+    /***********************************************************************************/
+    //optimized solution, time O(n), space O(1), but need to change the signature input array
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        //change input array, to mark the inrelevant number
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= 0 || nums[i] > nums.length) {
+                nums[i] = nums.length + 1;
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int abs = Math.abs(nums[i]);
+            if (abs <= nums.length) {
+                nums[abs - 1] = -Math.abs(nums[abs - 1]);
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
 }
