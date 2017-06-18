be22185ffd52e9d67508b909bea4219e8ba2ca96
abxcytf <lawrencelou88@gmail.com>
2017-06-18 04:08:41 -0400

    Update LeetCodeQ.186.Reverse_Words_in_a_String_II.java

--- Microsoft/LeetCodeQ.186.Reverse_Words_in_a_String_II.java
+++ Microsoft/LeetCodeQ.186.Reverse_Words_in_a_String_II.java
@@ -41,6 +41,28 @@ public class Solution {
     }
     
     /*************************************************************************************/
+    public void reverseWords(char[] s) {
+        if (s == null || s.length == 0) {
+            return;
+        }
+        reverse(s, 0, s.length - 1);
+        int i = 0;
+        for (int j = 0; j < s.length; j++) {
+            if (s[j] == ' ') {
+                reverse(s, i, j - 1);
+                i = j + 1;
+            }
+        }
+        reverse(s, i, s.length - 1);
+    }
     
-    
+    private void reverse(char[] s, int start, int end) {
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
