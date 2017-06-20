bc7199f4e81dbf990d99ee49be4a331dcef01203
abxcytf <lawrencelou88@gmail.com>
2017-06-20 15:40:28 -0400

    Update LeetCodeQ21.Merge_Two_Sorted_Lists.java

--- Microsoft/LeetCodeQ21.Merge_Two_Sorted_Lists.java
+++ Microsoft/LeetCodeQ21.Merge_Two_Sorted_Lists.java
@@ -14,6 +14,7 @@ The new list should be made by splicing together the nodes of the first two list
  * }
  */
 public class Solution {
+    //iteration
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null && l2 == null) {
             return null;
@@ -46,5 +47,24 @@ public class Solution {
         current.next = l1 == null ? l2 : l1;
         
         return dummy.next;
+    }
+    
+    /*****************************************************************************/
+    //recursion, top down implementation
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
     }
 }
