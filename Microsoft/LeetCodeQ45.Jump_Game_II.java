8d96bd684b3b2994624c1f75446478927051e9b2
abxcytf <lawrencelou88@gmail.com>
2017-06-28 18:12:15 -0400

    Create LeetCodeQ45.Jump_Game_II.java

---
+++ Microsoft/LeetCodeQ45.Jump_Game_II.java
@@ -0,0 +1,48 @@
+/*
+45. Jump Game II
+
+Given an array of non-negative integers, 
+you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+For example:
+Given array A = [2,3,1,1,4]
+
+The minimum number of jumps to reach the last index is 2. 
+(Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+
+Note:
+. You can assume that you can always reach the last index.
+*/
+
+public class Solution {
+    //DP implementation, time complexity: O(n^2), TLE in leetcode
+    public int jump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        //state
+        int[] steps = new int[nums.length];
+        
+        //initialize
+        steps[0] = 0;
+        for (int i = 1; i < nums.length; i++) {
+            steps[i] = Integer.MAX_VALUE; 
+        }
+        
+        //function
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (steps[j] != Integer.MAX_VALUE && j + nums[j] >= i) {
+                    steps[i] = Math.min(steps[i], steps[j] + 1);
+                }
+            }
+        }
+        
+        //answer
+        return steps[nums.length - 1];
+    }
+}
