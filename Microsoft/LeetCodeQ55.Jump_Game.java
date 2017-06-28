6246f1603b129d1c43b54aa789fe071f7f4204e6
abxcytf <lawrencelou88@gmail.com>
2017-06-28 17:57:13 -0400

    Update LeetCodeQ55.Jump_Game.java

--- Microsoft/LeetCodeQ55.Jump_Game.java
+++ Microsoft/LeetCodeQ55.Jump_Game.java
@@ -89,4 +89,18 @@ public class Solution {
         }
         return can[nums.length - 1];
     }
+    
+    /*******************************************************************************/
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int farthest = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (i <= farthest && i + nums[i] >= farthest) {
+                farthest = i + nums[i];
+            }
+        }
+        return farthest >= nums.length - 1;
+    }
 }
