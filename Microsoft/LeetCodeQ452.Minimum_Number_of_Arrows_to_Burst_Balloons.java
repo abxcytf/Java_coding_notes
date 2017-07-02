40a246703cff27a87e994585a42fc6d2058fba49
abxcytf <lawrencelou88@gmail.com>
2017-07-02 18:56:46 -0400

    Update LeetCodeQ452.Minimum_Number_of_Arrows_to_Burst_Balloons.java

--- Microsoft/LeetCodeQ452.Minimum_Number_of_Arrows_to_Burst_Balloons.java
+++ Microsoft/LeetCodeQ452.Minimum_Number_of_Arrows_to_Burst_Balloons.java
@@ -35,13 +35,15 @@ public class Solution {
         
         Arrays.sort(points, (a, b) -> (a[0] - b[0])); //sort with the starting position
         int result = 1;
+        //Sort according to the balloons start address
+        Arrays.sort(points, (a, b) -> (a[0] - b[0]));
         int end = points[0][1];
-        for (int i = 1; i < points.length; i++) {
-            if (points[i][0] > end) {
+        for (int i = 0; i < points.length; i++) {
+            if (points[i][0] <= end) {
+                end = Math.min(end, points[i][1]);
+            } else {
                 result++;
                 end = points[i][1];
-            } else {
-                end = Math.min(end, points[i][1]);
             }
         }
         return result;
