eb88b2d9edb9f181fa939887b73b0591d36e3c55
abxcytf <lawrencelou88@gmail.com>
2017-07-02 19:19:30 -0400

    Update LeetCodeQ452.Minimum_Number_of_Arrows_to_Burst_Balloons.java

--- Microsoft/LeetCodeQ452.Minimum_Number_of_Arrows_to_Burst_Balloons.java
+++ Microsoft/LeetCodeQ452.Minimum_Number_of_Arrows_to_Burst_Balloons.java
@@ -48,4 +48,24 @@ public class Solution {
         }
         return result;
     }
+    
+    /***************************************************************************/
+    //optimized solution, sort according to the end position, time complexity: O(n)
+    public int findMinArrowShots(int[][] points) {
+        if (points == null || points.length == 0 || points[0].length == 0) {
+            return 0;
+        }
+        Arrays.sort(points, (a, b) -> (a[1] - b[1]));
+        int end = points[0][1];
+        int result = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (end >= points[i][0]) {
+                continue;
+            } else {
+                result++;
+                end = points[i][1];
+            }
+        }
+        return result;
+    }
 }
