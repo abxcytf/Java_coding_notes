90249eb0e1e256dbd94d47ff3a8936a06eeb8fb1
abxcytf <lawrencelou88@gmail.com>
2017-07-05 15:14:07 -0400

    Update LeetCodeQ238.Product_of_Array_Except_Self.java

--- Week06/LeetCodeQ238.Product_of_Array_Except_Self.java
+++ Week06/LeetCodeQ238.Product_of_Array_Except_Self.java
@@ -13,7 +13,32 @@ Could you solve it with constant space complexity?
 */
 
 public class Solution {
-    //time: O(n), space: O(1)
+    //time complexity O(n), space complexity O(n)
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return nums;
+        }
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        int[] result = new int[nums.length];
+        
+        left[0] = 1;
+        right[nums.length - 1] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = left[i] * right[i];
+        }
+        return result;
+    }
+    
+    /***************************************************************************************************************/
+    //optimized solution, time: O(n), space: O(1)
     public int[] productExceptSelf(int[] nums) {
         if (nums == null || nums.length == 0) {
             return nums;
