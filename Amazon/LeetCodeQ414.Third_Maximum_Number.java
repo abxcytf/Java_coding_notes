9a28ab48dbff28e525c295dba4dab9d2c6aaa599
abxcytf <lawrencelou88@gmail.com>
2017-07-07 03:06:55 -0400

    Update LeetCodeQ414.Third_Maximum_Number.java

--- Amazon/LeetCodeQ414.Third_Maximum_Number.java
+++ Amazon/LeetCodeQ414.Third_Maximum_Number.java
@@ -60,4 +60,41 @@ public class Solution {
         }
         return max3 == null ? max1 : max3;
     }
+    
+    /************************************************************************************/
+    //same idea as above using long
+    public int thirdMax(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        if (nums.length == 2) {
+            return nums[0] > nums[1] ? nums[0] : nums[1];
+        }
+        
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
+    
+        for (int num : nums) {
+            if (max1 == num || max2 == num || max3 == num) {
+                continue;
+            }
+            if (max1 == Long.MIN_VALUE || max1 < num) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (max2 == Long.MIN_VALUE || max2 < num) {
+                max3 = max2;
+                max2 = num;
+            } else if (max3 == Long.MIN_VALUE || max3 < num) {
+                max3 = num;
+            }
+        }
+        return max3 == Long.MIN_VALUE ? (int)max1 : (int)max3;
+    }
 }
