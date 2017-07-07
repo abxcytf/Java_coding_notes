c1ae31f96c5049e5de80bf6d7377964e03edc9ec
abxcytf <lawrencelou88@gmail.com>
2017-07-07 03:03:52 -0400

    Create LeetCodeQ414.Third_Maximum_Number.java

---
+++ Amazon/LeetCodeQ414.Third_Maximum_Number.java
@@ -0,0 +1,63 @@
+/*
+414. Third Maximum Number
+
+Given a non-empty array of integers, return the third maximum number in this array. 
+If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+Example 1:
+Input: [3, 2, 1]
+
+Output: 1
+
+Explanation: The third maximum is 1.
+Example 2:
+Input: [1, 2]
+
+Output: 2
+
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+Input: [2, 2, 3, 1]
+
+Output: 1
+
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+*/
+
+public class Solution {
+    //three pointers, time complexity O(n)
+    public int thirdMax(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        if (nums.length == 2) {
+            return nums[0] > nums[1] ? nums[0] : nums[1];
+        }
+        
+        Integer max1 = null; 
+        Integer max2 = null;
+        Integer max3 = null;
+        for (Integer num : nums) {
+            if (num.equals(max1) || num.equals(max2) || num.equals(max3)) {
+                continue;
+            }
+            if (max1 == null || max1 < num) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (max2 == null || max2 < num) {
+                max3 = max2;
+                max2 = num;
+            } else if (max3 == null || max3 < num) {
+                max3 = num;
+            }
+        }
+        return max3 == null ? max1 : max3;
+    }
+}
