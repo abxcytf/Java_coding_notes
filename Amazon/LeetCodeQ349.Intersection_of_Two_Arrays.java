52f843acec27719a54826f60a95f3dce8c3d094d
abxcytf <lawrencelou88@gmail.com>
2017-07-09 03:16:41 -0400

    Update LeetCodeQ349.Intersection_of_Two_Arrays.java

--- Amazon/LeetCodeQ349.Intersection_of_Two_Arrays.java
+++ Amazon/LeetCodeQ349.Intersection_of_Two_Arrays.java
@@ -39,6 +39,37 @@ public class Solution {
     }
     
     /***************************************************************************************/
+    Sort both arrays, use two pointers, time complexity O(n), space complexity O(n)
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        int[] result = new int[set.size()];
+        int index = 0;
+        for (Integer num : set) {
+            result[index++] = num;
+        }
+        return result;
+    }
+    
+    /*********************************************************************************************/
     
     
 }
