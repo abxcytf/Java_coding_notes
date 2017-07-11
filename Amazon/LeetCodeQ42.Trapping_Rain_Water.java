ede42bbbdd23abce0b1c7640faf3139f637f0693
abxcytf <lawrencelou88@gmail.com>
2017-07-11 02:06:12 -0400

    Update LeetCodeQ42.Trapping_Rain_Water.java

--- Amazon/LeetCodeQ42.Trapping_Rain_Water.java
+++ Amazon/LeetCodeQ42.Trapping_Rain_Water.java
@@ -13,7 +13,7 @@ In this case, 6 units of rain water (blue section) are being trapped.
 */
 
 public class Solution {
-
+    //time complexity O(n^2)
     //brutal force, calculate on each storage of trapping water on each height
     public int trap(int[] height) {
         if (height == null || height.length <= 1) {
@@ -31,6 +31,42 @@ public class Solution {
                 rightMax = Math.max(rightMax, height[r]);
             }
             result += Math.max(0, Math.min(leftMax, rightMax) - height[i]);
+        }
+        return result;
+    }
+    
+    
+    /************************************************************************************/
+    //two pointers, optimized solution, time complexity O(n)
+    public int trap(int[] height) {
+        if (height == null || height.length <= 1) {
+            return 0;
+        }
+        int left = 0;
+        int right = height.length - 1;
+        int leftHeight = height[left];
+        int rightHeight = height[right];
+        int result = 0;
+        while (left < right) {
+            if (leftHeight < rightHeight) {
+                left++;
+                if (leftHeight > height[left]) {
+                    //if leftHeight > height[left], update the result
+                    result += leftHeight - height[left];
+                } else {
+                    //if leftHeight < height[left], update the leftHeight to larger one
+                    leftHeight = height[left];
+                }
+            } else {
+                right--;
+                if (rightHeight > height[right]) {
+                    //if rightHeight > height[right], update the result
+                    result += rightHeight - height[right];
+                } else {
+                    //if rightHeight < height[right], update the rightHeight to larger one
+                    rightHeight = height[right];
+                }
+            }
         }
         return result;
     }
