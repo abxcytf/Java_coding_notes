d0311a497f0975cd8631735751b4ecf84157a353
abxcytf <lawrencelou88@gmail.com>
2017-07-11 01:37:41 -0400

    Update LeetCodeQ118.Pascal's_Triangle.java

--- Amazon/LeetCodeQ118.Pascal's_Triangle.java
+++ Amazon/LeetCodeQ118.Pascal's_Triangle.java
@@ -49,6 +49,26 @@ public class Solution {
     }
     
     /*************************************************************/
-    
-    
+    /*
+    i = 0 {1} =>              {1}
+    i = 1 {1,1} =>           {1,1}
+    i = 2 {1,1,1} =>        {1,2,1}
+    i = 3 {1,1,2,1} =>     {1,3,3,1}
+    i = 4 {1,1,3,3,1} =>  {1,4,6,4,1}
+    */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows < 1) {
+            return result;
+        }
+        List<Integer> row = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+            result.add(new ArrayList<>(row));
+        } 
+        return result;
+    }
 }
