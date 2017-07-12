b5c16f96a4811c8c13e74178c9bf09e7b43fd4f0
abxcytf <lawrencelou88@gmail.com>
2017-07-12 00:00:24 -0400

    Update LeetCodeQ396.Rotate_Function.java

--- Amazon/LeetCodeQ396.Rotate_Function.java
+++ Amazon/LeetCodeQ396.Rotate_Function.java
@@ -64,26 +64,3 @@ public class Solution {
         return best;
     }
 }
-
-public class Solution {
-    public int maxRotateFunction(int[] A) {
-        if (A == null || A.length == 0) {
-            return 0;
-        }
-        int n = A.length;
-        int sum = 0;
-        int candidate = 0;
-        
-        for (int i = 0; i < n; i++) {
-            sum += A[i];
-            candidate += i * A[i];
-        }
-        
-        int best = candidate;
-        for (int i = n - 1; i >= 0; i--) {
-            candidate = candidate + sum - n * A[i];
-            best = Math.max(best, candidate);
-        }
-        return best;
-    }
-}
