89b6c65d05170e23aa66a92273e7eba36c6a439c
abxcytf <lawrencelou88@gmail.com>
2017-07-11 23:09:16 -0400

    Update LeetCodeQ516.Longest_Palindromic_Subsequence.java

--- Amazon/LeetCodeQ516.Longest_Palindromic_Subsequence.java
+++ Amazon/LeetCodeQ516.Longest_Palindromic_Subsequence.java
@@ -43,4 +43,25 @@ public class Solution {
         }
         return memo[i][j];
     }
+    
+    /****************************************************************************************/
+    //dp implementation
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = s.length() - 1; i >= 0 ; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.length() - 1];
+    }
 }
