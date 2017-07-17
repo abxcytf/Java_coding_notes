e640aa1bea0b1113c41d7102b24e4af49c591584
abxcytf <lawrencelou88@gmail.com>
2017-07-17 15:36:27 -0400

    Update LeetCodeQ459.Repeated_Substring_Pattern.java

--- Amazon/LeetCodeQ459.Repeated_Substring_Pattern.java
+++ Amazon/LeetCodeQ459.Repeated_Substring_Pattern.java
@@ -23,16 +23,40 @@ Output: True
 
 Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
 */
-/*
-维护的一位数组dp[i]表示，到位置i-1为止的重复字符串的字符个数，不包括被重复的那个字符串，
-我们举个例子，比如"abcabc"的dp数组为[0 0 0 0 1 2 3]，dp数组长度要比原字符串长度多一个。
-那么我们看最后一个位置数字为3，就表示重复的字符串的字符数有3个。如果是"abcabcabc"，
-那么dp数组为[0 0 0 0 1 2 3 4 5 6]，我们发现最后一个数字为6，那么表示重复的字符串为“abcabc”，
-有6个字符。那么怎么通过最后一个数字来知道原字符串是否由重复的子字符串组成的呢，
-首先当然是最后一个数字不能为0，而且还要满足dp[n] % (n - dp[n]) == 0才行，
-因为n - dp[n]是一个子字符串的长度，那么重复字符串的长度和肯定是一个子字符串的整数倍。
-*/
+
 public class Solution {
+    //brutal force, TLE
+    public boolean repeatedSubstringPattern(String s) {
+        if (s == null || s.length() == 0) {
+            return false;
+        }
+        
+        int len = s.length();
+        for (int i = len / 2; i >= 1; i--) {
+            if (len % i == 0) {
+                int count = len / i;
+                String unit = "";
+                for (int j = 0; j < count; j++) {
+                    unit += s.substring(0, i);
+                }
+                if (unit.equals(s)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    /*********************************************************************************************/
+    /*
+    维护的一位数组dp[i]表示，到位置i-1为止的重复字符串的字符个数，不包括被重复的那个字符串，
+    我们举个例子，比如"abcabc"的dp数组为[0 0 0 0 1 2 3]，dp数组长度要比原字符串长度多一个。
+    那么我们看最后一个位置数字为3，就表示重复的字符串的字符数有3个。如果是"abcabcabc"，
+    那么dp数组为[0 0 0 0 1 2 3 4 5 6]，我们发现最后一个数字为6，那么表示重复的字符串为“abcabc”，
+    有6个字符。那么怎么通过最后一个数字来知道原字符串是否由重复的子字符串组成的呢，
+    首先当然是最后一个数字不能为0，而且还要满足dp[n] % (n - dp[n]) == 0才行，
+    因为n - dp[n]是一个子字符串的长度，那么重复字符串的长度和肯定是一个子字符串的整数倍。
+    */
     public boolean repeatedSubstringPattern(String s) {
         if (s == null || s.length() == 0) {
             return false;
