e23af497e659537378542a4805801c2ce6ca16bc
abxcytf <lawrencelou88@gmail.com>
2017-07-20 13:42:04 -0400

    Update LeetCodeQ29.Divide_Two_Integers.java

--- Amazon/LeetCodeQ29.Divide_Two_Integers.java
+++ Amazon/LeetCodeQ29.Divide_Two_Integers.java
@@ -37,4 +37,37 @@ public class Solution {
         }
         return isNegative? -result : result;
     }
+    
+    /*****************************************************************************************************/
+    //optimized solution
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0) {
+            //分母为0的情况
+            return dividend >= 0? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+        
+        if (dividend == 0) {
+            //分子为0的情况
+            return 0;
+        }
+        
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            //another special case need to discuss seperately
+            return Integer.MAX_VALUE;
+        }
+        
+        boolean isNegative = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
+        int result = 0;
+        for (int i = 31; i >= 0; i--) {
+            //比较a是否大于b的(1 << i)次方，避免将a与(b << i)比较
+            //因为不确定b的(b << i)次方是否溢出
+            if ((a >> i) >= b) {
+                result += (1 << i);
+                a -= (b << i);
+            }
+        }
+        return isNegative? -result : result;
+    }
 }
