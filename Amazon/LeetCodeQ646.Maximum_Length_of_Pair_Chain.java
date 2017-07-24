c16d8973aaf6fb07cecf2ab9de5c168918bfdd9f
abxcytf <lawrencelou88@gmail.com>
2017-07-24 15:00:48 -0400

    Update LeetCodeQ646.Maximum_Length_of_Pair_Chain.java

--- Amazon/LeetCodeQ646.Maximum_Length_of_Pair_Chain.java
+++ Amazon/LeetCodeQ646.Maximum_Length_of_Pair_Chain.java
@@ -3,9 +3,11 @@
 
 You are given n pairs of numbers. In every pair, the first number is always smaller than the second number.
 
-Now, we define a pair (c, d) can follow another pair (a, b) if and only if b < c. Chain of pairs can be formed in this fashion.
+Now, we define a pair (c, d) can follow another pair (a, b) if and only if b < c. Chain of pairs can 
+be formed in this fashion.
 
-Given a set of pairs, find the length longest chain which can be formed. You needn't use up all the given pairs. You can select pairs in any order.
+Given a set of pairs, find the length longest chain which can be formed. You needn't use up all the given pairs. 
+You can select pairs in any order.
 
 Example 1:
 Input: [[1,2], [2,3], [3,4]]
@@ -22,7 +24,7 @@ public class Solution {
             return 0;
         }
         
-        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]); //O(nlogn);
         int result = 1;
         int currentEnd = pairs[0][1];
         for (int i = 1; i < pairs.length; i++) {
@@ -33,4 +35,6 @@ public class Solution {
         }
         return result;
     }
+    
+    /**************************************************************************************************/
 }
