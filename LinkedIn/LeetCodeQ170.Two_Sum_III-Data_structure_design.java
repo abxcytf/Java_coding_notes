1e185bcfc0701274a4c68c7900849a3b1487365f
abxcytf <lawrencelou88@gmail.com>
2017-07-26 15:19:23 -0400

    Update LeetCodeQ170.Two_Sum_III-Data_structure_design.java

--- LinkedIn/LeetCodeQ170.Two_Sum_III-Data_structure_design.java
+++ LinkedIn/LeetCodeQ170.Two_Sum_III-Data_structure_design.java
@@ -14,6 +14,38 @@ find(7) -> false
 
 public class TwoSum {
     
+    Map<Integer, Integer> map;
+    
+    /** Initialize your data structure here. */
+    public TwoSum() {
+        map = new HashMap<Integer, Integer>();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        if (map.containsKey(number)) {
+            map.put(number, 2);
+        } else {
+            map.put(number, 1);
+        }
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        Iterator<Integer> itr = map.keySet().iterator();
+        while (itr.hasNext()) {
+            int num1 = itr.next();
+            int num2 = value - num1;
+            if (map.containsKey(num2)) {
+                if (num1 != num2 || map.get(num2) == 2) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    /********************************************************************************************************/
+    
     Set<Integer> sum;
     Set<Integer> num;
     
