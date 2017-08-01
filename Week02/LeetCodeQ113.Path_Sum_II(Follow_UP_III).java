8a59ec548ab81c9677e47fc375ddd426bb278460
abxcytf <lawrencelou88@gmail.com>
2017-08-01 00:42:28 -0400

    Create LeetCodeQ113.Path_Sum_II(Follow_UP_III).java

---
+++ Week02/LeetCodeQ113.Path_Sum_II(Follow_UP_III).java
@@ -0,0 +1,26 @@
+/*
+Follow Up III: Path -> Any node to any other node
+*/
+
+public class Solution {
+    //time complexity: O(n), space complexity O(1)
+    public int maxPathSum(TreeNode root, int sum) {
+        int[] max = {Integer.MIN_VALUE};
+        dfsHelper(root, max);
+        return max[0];
+    }
+    
+    private int dfsHelper(TreeNode root, int[] max) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfsHelper(root.left, max);
+        int right = dfsHelper(root.right, max);
+        
+        left = Math.max(left, 0);
+        right = Math.max(right, 0);
+        //update the global max
+        max[0] = Math.max(max[0], left + right + root.val);   //the only difference from follow up II
+        return Math.max(left, right) + root.val; //return the larger side
+    }
+}
