334b77fe7c7b8e9b92bbcc511b2b4bbacad9deed
abxcytf <lawrencelou88@gmail.com>
2017-08-01 10:44:26 -0400

    Update LeetCodeQ113.Path_Sum_II(Follow_UP_IV).java

--- Week02/LeetCodeQ113.Path_Sum_II(Follow_UP_IV).java
+++ Week02/LeetCodeQ113.Path_Sum_II(Follow_UP_IV).java
@@ -4,7 +4,7 @@ Follow Up IV: Path -> from leaf node to other leaf node
 
 public class Solution {
     //time complexity: O(n), space complexity O(1)
-    public int maxPathSum(TreeNode root, int sum) {
+    public int maxPathSum(TreeNode root) {
         int[] max = {Integer.MIN_VALUE};
         dfsHelper(root, max);
         return max[0];
