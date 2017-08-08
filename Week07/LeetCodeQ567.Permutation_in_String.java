7b7e50b8573d55ab11839aeecd0b20e4ffd8147a
abxcytf <lawrencelou88@gmail.com>
2017-08-08 02:04:35 -0400

    Update LeetCodeQ567.Permutation_in_String.java

--- Week07/LeetCodeQ567.Permutation_in_String.java
+++ Week07/LeetCodeQ567.Permutation_in_String.java
@@ -57,4 +57,54 @@ public class Solution {
         }
         return true;
     }
+    
+    /******************************************************************************************************/
+    //import java.math.BigInteger;
+    public boolean checkInclusion(String s1, String s2) {
+        //corner case
+        if (s1 == null || s1.length() == 0) {
+            return true;
+        }
+        if (s2 == null || s2.length() == 0) {
+            return false;
+        }
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        char[] chOne = s1.toCharArray();
+        char[] chTwo = s2.toCharArray();
+        
+        //long max will overflow at 9 digits
+        BigInteger target = new BigInteger("1");
+        BigInteger checkSum = new BigInteger("1");
+        
+        //compose the big prime number
+        //create the first window
+        for (int i = 0; i < chOne.length; i++) {
+            int nOne = chOne[i] - 'a';
+            BigInteger primeOne = new BigInteger(Integer.toString(nOne * nOne + nOne + 41));
+            int nTwo = chTwo[i] - 'a';
+            BigInteger primeTwo = new BigInteger(Integer.toString(nTwo * nTwo + nTwo + 41));
+            target = target.multiply(primeOne);
+            checkSum = checkSum.multiply(primeTwo);
+        }
+        
+        if (target.equals(checkSum)) {
+            return true;
+        }
+        
+        for (int i = chOne.length; i < chTwo.length; i++) {
+            int nLast = chTwo[i - chOne.length] - 'a';
+            BigInteger primeLast = new BigInteger(Integer.toString(nLast * nLast + nLast + 41));
+            int nNext = chTwo[i] - 'a';
+            BigInteger primeNext = new BigInteger(Integer.toString(nNext * nNext + nNext + 41));
+            checkSum = checkSum.divide(primeLast);
+            checkSum = checkSum.multiply(primeNext);
+            
+            if (target.equals(checkSum)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
